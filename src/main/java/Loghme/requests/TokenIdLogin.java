05f36ac3eb740d22d73fae48586c854cd9ea7d69
Amin Asadi <aminasadi329@gmail.com>
2020-05-07 22:57:13 +0430

    finish tocken id

---
+++ src/main/java/Loghme/requests/TokenIdLogin.java
@@ -0,0 +1,14 @@
+package Loghme.requests;
+
+public class TokenIdLogin {
+    public String getTokenId() {
+        return tockenId;
+    }
+
+    public void setTokenId(String tokenId) {
+        this.tockenId = tokenId;
+    }
+
+    private String tockenId;
+
+}
